9df371bd2096efe636a68a5ab4f2b006f2f3e61a
Richard Hanolt <rhanolt@gmail.com>
2020-05-02 20:50:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   new_array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array
-
+def add_element_to_end_of_array(array, element)
+  array << element
 end
 
 def add_element_to_start_of_array
